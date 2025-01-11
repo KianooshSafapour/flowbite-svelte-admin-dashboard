0c2ac82babe2b56c77fd3c7d3b7596e528f8eaef
Kianoosh Safapour <kianoosh.safapour@gmail.com>
2025-01-11 14:05:15 +0330

    no

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,5 @@
-<script>
-	import modeobserver from './utils/modeobserver';
-	import { onMount } from 'svelte';
-	import Runatics from './utils/Runatics.svelte';
-  export let data;
-  const analyticsId = data.ANALYTICS_ID
-
-	onMount(modeobserver);
+<script lang="ts">
+	import '../app.css';
 </script>
-<Runatics {analyticsId} />
+
 <slot />
