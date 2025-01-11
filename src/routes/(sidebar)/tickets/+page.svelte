7abc6d5269e5eea9beb0dc20ba31b50ff888da5a
Kianoosh Safapour <kianoosh.safapour@gmail.com>
2025-01-11 14:26:26 +0330

    ((

--- src/routes/(sidebar)/tickets/+page.svelte
+++ src/routes/(sidebar)/tickets/+page.svelte
@@ -4,12 +4,6 @@
 
 <main class="p-4">
 	<div class="mt-px space-y-4">
-		<div class="grid grid-cols-6 gap-4">
-			<Card horizontal class="items-center justify-between" size="xl">
-				<div class="w-full"></div></Card
-			>
-			<Card horizontal class="items-center justify-between" size="xl"></Card>
-			<Card horizontal class="items-center justify-between" size="xl"></Card>
-		</div>
+		<div class="grid grid-cols-6 gap-4"></div>
 	</div>
 </main>
